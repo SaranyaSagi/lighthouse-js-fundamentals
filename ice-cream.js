a4726ccd088657785172f9286837d721ca3d5285
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-15 16:39:56 -0500

    practice with arrays

--- ice-cream.js
+++ ice-cream.js
@@ -1,5 +1,7 @@
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry", "root beer"];
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
 console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours.push("root beer"));
 
 console.log(iceCreamFlavours[0]); //first item in array
 
