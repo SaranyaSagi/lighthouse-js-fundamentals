9f20bcbf6acbd52243a50767125307c88296c85d
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-15 10:43:00 -0500

    add function to say hello with name parameter

---
+++ hello.js
@@ -0,0 +1,11 @@
+const sayHello = function () {
+ console.log("Hello, world");
+}
+sayHello();
+
+const sayHellos = function(name) {
+  console.log("Hello, " + name);
+}
+sayHellos("Caleb");
+sayHellos("Miranda");
+sayHellos("Ferdinand");
