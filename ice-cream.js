9f9fff18556b9d52d2fbaffe4b3b697ee2e96977
SaranyaSagi <saranya108sagi@gmail.com>
2021-11-15 14:34:00 -0500

    add items to array and use index to console log and use length to find items in array

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry", "root beer"];
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]); //first item in array
+
+console.log(iceCreamFlavours[5]); //last item in array
+
+console.log(iceCreamFlavours.length);
